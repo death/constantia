aebf43aedde274d756414d5a4a8bf65c26ce5ce6
death <death@nessers.org>
2014-12-11 23:38:23 +0200

    out: don't outs the :h separator form
    It's better to be consistent with :s separator parameter and friends.
    The user can always pass an (outs ...) form herself.

--- out.lisp
+++ out.lisp
@@ -303,7 +303,7 @@
     (let ((first (gensym))
           (separator-value (gensym)))
       `(:forms (let ((,first t)
-                     (,separator-value ,(if separator `(outs ,separator) nil)))
+                     (,separator-value ,separator))
                  (maphash (lambda (,k ,v)
                             (declare (ignorable ,k ,v))
                             (cond (,first (setf ,first nil))
